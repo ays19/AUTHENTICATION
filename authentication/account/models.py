37c0193a517c9bc8a4c47a28019b1ef5522e185d
ays19 <sahsanyasir@gmail.com>
2025-11-30 08:51:29 +0600

    changes in models.py & settinngs.py

--- authentication/account/models.py
+++ authentication/account/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
+from .managers import UserManager
 
 class User(AbstractBaseUser, PermissionsMixin):
 	username= models.CharField(
@@ -18,7 +19,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 	is_superuser= models.BooleanField(default=False)
 	is_active= models.BooleanField(default=True)
 	date_joined= models.DateTimeField(auto_now_add=True)
-	objects= None
+	objects= UserManager()
 
 	USERNAME_FIELD = "username"
 	REQUIRED_FIELDS = ["email", ]
