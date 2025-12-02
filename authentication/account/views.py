94b21717cdc7f816747c6a68f973b6d2c9a2d297
ays19 <sahsanyasir@gmail.com>
2025-12-02 09:00:06 +0600

    change in views.py add login

--- authentication/account/views.py
+++ authentication/account/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 
 class Home(generic.TemplateView):
 	template_name= "account/home.html"
+
+class Login(generic.View):
+	def get(self, *args, **kwargs):
+		return render(self.request, 'account/login.html')
+
+	def post(self, *args, **kwargs):
+		pass	
