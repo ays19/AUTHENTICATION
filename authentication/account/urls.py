2fe5c41753cf7805276a70305b1eef24f9eba983
Ahsan Yasir Sharar <sahsanyasir@gmail.com>
2025-12-03 14:51:40 +0600

    login change in urls.py

--- authentication/account/urls.py
+++ authentication/account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import (
-    Home
+    Home,
+    Login
 )
 
 urlpatterns= [
     path('', Home.as_view(), name='home'),
+    path('login/', Login.as_view(), name='login'),
 ]
