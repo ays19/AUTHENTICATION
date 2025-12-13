3607724f6eff62bd530590fb5edfdb7a88a57b1d
ays19 <sahsanyasir@gmail.com>
2025-12-14 05:33:00 +0600

    home.html new code

--- authentication/account/urls.py
+++ authentication/account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import (
     Home,
-    Login
+    Login,
+    Logout
 )
 
 urlpatterns= [
--- authentication/account/views.py
+++ authentication/account/views.py
@@ -6,6 +6,10 @@ from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse_lazy
 
 
+from .mixins import (
+    LogoutRequiredMixin,
+)
+
 from .forms import (
     LoginForm,
         
@@ -17,7 +21,7 @@ class Home(LoginRequiredMixin, generic.TemplateView):
     template_name = "account/home.html"
 
 
-class Login(generic.View):
+class Login(LogoutRequiredMixin,generic.View):
     def get(self, *args, **kwargs):
         form = LoginForm()
         context = {
@@ -25,26 +29,25 @@ class Login(generic.View):
         }
         return render(self.request, 'account/login.html', context)
     
+def post(self, *args, **kwargs):
+		form = LoginForm(self.request.POST)
+		if form.is_valid():
+			user = authenticate(
+					self.request,
+					username=form.cleaned_data.get('username'),
+					password=form.cleaned_data.get('password')
+				)
+			if user:
+					login(self.request, user)
+					return redirect('home')
+			else:	
+				forms.ValidationError("Incorrect password")
+				return redirect('login')
+		return render(self.request, 'account/login.html', {"form": form})
+    
 class Logout(generic.View):
     def get(self, *args, **kwargs):
         logout(self.request)
         return redirect('login')    
 
-    def post(self, *args, **kwargs):
-        form = LoginForm(self.request.POST)
-
-        if form.is_valid():
-            user = authenticate(
-                self.request,
-                username=form.cleaned_data.get('username'),
-                password=form.cleaned_data.get('password')
-            )
-            if user:
-                login(self.request, user)
-                return redirect('home')
-
-            else:
-                messages.warning(self.request, "Wrong credentials")
-                return redirect('login')
-
-        return render(self.request, 'account/login.html', {"form": form})
+   
