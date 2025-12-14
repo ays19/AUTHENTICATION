36dece0e65f5e0f454c897ba7961042832e62753
ays19 <sahsanyasir@gmail.com>
2025-12-14 08:07:34 +0600

    spelling mistake solved

--- authentication/authentication/settings.py
+++ authentication/authentication/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'authetication.urls'
+ROOT_URLCONF = 'authentication.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'authetication.wsgi.application'
+WSGI_APPLICATION = 'authentication.wsgi.application'
 
 
 # Database
